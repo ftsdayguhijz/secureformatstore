c82a5b32545ab15a5a9eb847ee6a97030ecb6b89
ftsdayguhijz <hsjcansmiguel23@hotmail.com>
2024-05-07 14:55:26 +0700

    init

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-function permuteUnique(nums) {
-  const result = [];
-  nums.sort((a, b) => a - b);
-  backtrack([], new Array(nums.length).fill(false));
-  return result;
-  function backtrack(current, used) {
-    if (current.length === nums.length) {
-      result.push([...current]);
-      return;
-    }
-    for (let i = 0; i < nums.length; i++) {
-      if (used[i] || (i > 0 && nums[i] === nums[i - 1] && !used[i - 1]))
-        continue;
-      current.push(nums[i]);
-      used[i] = true;
-      backtrack(current, used);
-      current.pop();
-      used[i] = false;
-    }
+function isSymmetric(root) {
+  if (!root) return true;
+  return isMirror(root.left, root.right);
+  function isMirror(left, right) {
+    if (!left && !right) return true;
+    if (!left || !right || left.val !== right.val) return false;
+    return isMirror(left.left, right.right) && isMirror(left.right, right.left);
   }
 }
